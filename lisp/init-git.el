16c23e6c2b9b494faa39ae75db3f65d684a7e398
chenyangm <mcy1208203968@hotmail.com>
2026-02-28 14:58:05 +0900

    feat: 追加关于git todo相关的配置

--- lisp/init-git.el
+++ lisp/init-git.el
@@ -13,6 +13,27 @@
   (magit-diff-refine-hunk t)
   (magit-ediff-dwim-show-on-hunks t))
 
+;; Show TODOs in Magit
+(use-package magit-todos
+  :ensure t
+  :after magit-status
+  :commands magit-todos-mode
+  :init
+  (setq magit-todos-nice (if (executable-find "nice") t nil))
+  (magit-todos-mode 1))
+
+(use-package diff-hl
+  :ensure t
+  :hook ((dired-mode         . diff-hl-dired-mode-unless-remote)
+         (magit-pre-refresh  . diff-hl-magit-pre-refresh)
+         (magit-post-refresh . diff-hl-magit-post-refresh))
+  :init
+  (global-diff-hl-mode t)
+  :config
+  ;; When Emacs runs in terminal, show the indicators in margin instead.
+  (unless (display-graphic-p)
+    (diff-hl-margin-mode)))
+
 (provide 'init-git)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; init-git.el ends here
